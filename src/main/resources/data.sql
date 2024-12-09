a9594c49a871f93361fba99def9e077d2edab099
Calebe-Dev <calebe.araujo101@gmail.com>
2024-12-09 08:45:24 -0300

    Editando data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 -- Inserindo dados de exemplo na tabela 'compromisso'
-INSERT INTO compromisso (id, title, description, compromisso_date, start_time, end_time, compromisso_status, local, priority) 
-VALUES (1, 'Reunião de Projeto', 'Discussão sobre o andamento do projeto X', '2024-11-20', '10:00:00', '11:00:00','Pendente', 'Sala de Reuniões 1', 2);
+INSERT INTO compromisso (id, title, description, compromisso_date, start_time, end_time, local, compromisso_status, priority) 
+VALUES (1, 'Comcluir Tarefa da Prog Web', 'Comcluir criação de exeplos', '2024-12-09', '07:40:00', '23:00:00', 'Qualquer lugar','Pendente', 10);
 
 INSERT INTO compromisso (id, title, description, compromisso_date, start_time, end_time, local, compromisso_status, priority) 
-VALUES (2, 'Consulta Médica', 'Consulta de rotina com o Dr. Silva', '2024-11-21', '15:00:00', '16:00:00','Pendente', 'Clínica São José', 1);
+VALUES (2, 'Subir tarefa para AWS', 'Rezar para que de certo, Tirar duvida sobre nomeação de subdominio', '2024-12-10', '07:40:00', '11:10:00','Pendente', 'Lab 6', 10);
 
 INSERT INTO compromisso (id, title, description, compromisso_date, start_time, end_time, local, compromisso_status, priority) 
-VALUES (3, 'Aula de Yoga', 'Sessão de yoga para relaxamento', '2024-11-22', '18:00:00', '19:00:00','Pendente', 'Academia Zen', 3);
+VALUES (3, 'Implorar por nota', 'Pedir encarecidamente para professor me dar uma nota legal ;-) !! ', '2024-12-10', '9:20:00', '11:10:00','Pendente', 'lab 6', 3);
